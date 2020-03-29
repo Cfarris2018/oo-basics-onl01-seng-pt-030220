1d41eee3c793a4561a8fc98aa6bbf7d6c40b1eda
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-29 20:11:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -23,20 +23,20 @@
   #end
   
  # def page_count
-    @page_count
-  end
+   # @page_count
+ # end
   
-  def genre=(genre)
-    @genre = genre
-  end
+  #def genre=(genre)
+    #@genre = genre
+ # end
   
-  def genre
-    @genre
-  end
+ # def genre
+   # @genre
+ # end
   
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+ # def turn_page
+   # puts "Flipping the page...wow, you read fast!"
+  #end
   
   
 end
