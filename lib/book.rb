611c95656d0d74a58e77238d08db802ffdb4dcb8
Camisha  Farris <cfarris@anointedflooring.com>
2020-03-29 20:11:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,28 +1,28 @@
 
 
-class Book
+#class Book
   
-  def initialize(title)
-    @title = title
-  end
+ #def initialize(title)
+    #@title = title
+  #end
   
-  def title
-    @title
-  end
+ # def title
+ #@title
+ # end
 
-  def author=(author)
-    @author = author
-  end
+  #def author=(author)
+    #@author = author
+  #end
   
-  def author
-    @author
-  end
+  #def author
+    #@author
+  #end
   
-  def page_count=(num)
-    @page_count = num
-  end
+  #def page_count=(num)
+   # @page_count = num
+  #end
   
-  def page_count
+ # def page_count
     @page_count
   end
   
